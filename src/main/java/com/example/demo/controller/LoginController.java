bad80cd8c1a4c0786682f0e768b805e722c4eb11
Quỳnh Em <anhptqth05962@gmail.com>
2026-03-19 13:48:13 +0700

    update

--- src/main/java/com/example/demo/controller/LoginController.java
+++ src/main/java/com/example/demo/controller/LoginController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class LoginController {
@@ -13,8 +14,20 @@ public class LoginController {
     }
 
     @PostMapping("/login")
-    public void login(
-            @RequestParam("username")  String username){
-        System.out.println("Ban vua nhap vao: " + username);
+    public String login(
+            @RequestParam("username")  String username,
+            @RequestParam("password")  String password,
+            //Truyền dự liệu qua 2 request
+            RedirectAttributes redirectAttributes) {
+
+        if ("admin".equals(username) && "admin".equals(password)) {
+            //addFlashAttribute lưu thông tin tạm thời vào Session
+            redirectAttributes.addFlashAttribute("username", username);
+            redirectAttributes.addFlashAttribute("password", password);
+            return "redirect:/list-user";
+
+        } else{
+            return "error.html";
+        }
     }
 }
--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -5,6 +5,8 @@ import com.example.demo.model.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 
@@ -27,24 +29,51 @@ public class UserController {
         return "index.html";
     }
 
+    ArrayList<User> users = new ArrayList<>();
+
+    public UserController(){
+        users.add(new User(1, "anhptqth05962@gmail.com", "123456", "Phạm Thị Quỳnh Anh", "0337376926", true, true));
+        users.add(new User(2, "anhptqth05962@gmail.com", "123456", "Phạm Thị Quỳnh Anh", "0346112846", true, false));
+        users.add(new User(3, "anhptqth05962@gmail.com", "123456", "Phạm Thị Quỳnh Anh", "0335223426", false, true));
+        users.add(new User(4, "anhptqth05962@gmail.com", "123456", "Phạm Thị Quỳnh Anh", "0948998656", true, true));
+        users.add(new User(5, "anhptqth05962@gmail.com", "123456", "Phạm Thị Quỳnh Anh", "0997277009", true, true));
+    }
     @GetMapping("/list-user")
     public String listUser(Model model){
-        ArrayList<User> users = new ArrayList<>();
-        users.add(new User(1, "anhptqth05962@gmail.com", "123456", "Phạm Thị Quỳnh Anh", "0337376926", true, true));
-        users.add(new User(2, "anhptqth05962@gmail.com", "123456", "Phạm Thị Quỳnh Anh", "0337376926", true, false));
-        users.add(new User(3, "anhptqth05962@gmail.com", "123456", "Phạm Thị Quỳnh Anh", "0337376926", false, true));
-        users.add(new User(4, "anhptqth05962@gmail.com", "123456", "Phạm Thị Quỳnh Anh", "0337376926", true, true));
-        users.add(new User(5, "anhptqth05962@gmail.com", "123456", "Phạm Thị Quỳnh Anh", "0337376926", true, true));
         // truyen danh sach user cho UI
         model.addAttribute("users", users);
 
-        ArrayList<NhanVien> nhanViens = new ArrayList<>();
-        nhanViens.add(new NhanVien(1, "nhanvien1@gmail.com", "123456789", "Nguyen Van A", 30.000,"Cau Giay", true));
-        nhanViens.add(new NhanVien(2, "nhanvien1@gmail.com", "123456789", "Nguyen Van A", 30.000,"Cau Giay", true));
-        nhanViens.add(new NhanVien(3, "nhanvien1@gmail.com", "123456789", "Nguyen Van A", 30.000,"Cau Giay", true));
-        nhanViens.add(new NhanVien(4, "nhanvien1@gmail.com", "123456789", "Nguyen Van A", 30.000,"Cau Giay", true));
-        nhanViens.add(new NhanVien(5, "nhanvien1@gmail.com", "123456789", "Nguyen Van A", 30.000,"Cau Giay", true));
-        model.addAttribute("lstNhanVien", nhanViens);
+//        ArrayList<NhanVien> nhanViens = new ArrayList<>();
+//        nhanViens.add(new NhanVien(1, "nhanvien1@gmail.com", "123456789", "Nguyen Van A", 30.000,"Cau Giay", true));
+//        nhanViens.add(new NhanVien(2, "nhanvien1@gmail.com", "123456789", "Nguyen Van A", 30.000,"Cau Giay", true));
+//        nhanViens.add(new NhanVien(3, "nhanvien1@gmail.com", "123456789", "Nguyen Van A", 30.000,"Cau Giay", true));
+//        nhanViens.add(new NhanVien(4, "nhanvien1@gmail.com", "123456789", "Nguyen Van A", 30.000,"Cau Giay", true));
+//        nhanViens.add(new NhanVien(5, "nhanvien1@gmail.com", "123456789", "Nguyen Van A", 30.000,"Cau Giay", true));
+//        model.addAttribute("lstNhanVien", nhanViens);
         return "index.html";
+    }
+
+    @GetMapping("/detail")
+    public String detail(Model model, @RequestParam("id") Integer id ){
+        User result = new User();
+        for (User u : users){
+            if(u.getId().equals(id)){
+                result = u;
+            }
+        }
+        model.addAttribute("user", result);
+        return "user-detail.html";
+    }
+
+    @PostMapping("/search")
+    public String search(Model model, @RequestParam("phone") String phone){
+        User result = new User();
+        for (User u : users){
+            if(u.getPhone().equals(phone)){
+                result = u;
+            }
+        }
+        model.addAttribute("user", result);
+        return "user-detail.html";
     }
 }
