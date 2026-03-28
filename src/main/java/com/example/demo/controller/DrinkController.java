d74b52d22084b33865686d2f22aac320de14dcfd
Quỳnh Em <anhptqth05962@gmail.com>
2026-03-28 23:17:13 +0700

    update crud table drink

--- src/main/java/com/example/demo/controller/DrinkController.java
+++ src/main/java/com/example/demo/controller/DrinkController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -31,6 +32,28 @@ public class DrinkController {
     public String addDrink(Drink drink, Model model) {
         System.out.println(drink.toString());
         model.addAttribute("category", new Category());
+        drinkRepo.save(drink);
+        return "redirect:/drink";
+    }
+
+    @GetMapping("/delete-drink")
+    public String deleteDrink(Drink drink) {
+        drinkRepo.delete(drink);
+        return "redirect:/drink";
+    }
+
+    @GetMapping("/detail-drink")
+    public String detailDrink(Model model, @RequestParam("id") Integer id) {
+        Drink d = drinkRepo.findById(id).orElse(null);
+        model.addAttribute("drink", d);
+        model.addAttribute("listCategory", categoryRepo.findAll());
+        model.addAttribute("drinks", drinkRepo.findAll());
+
+        return "drink-detail";
+    }
+
+    @PostMapping("/update-drink")
+    public String updateDrink(Drink drink, Model model) {
         drinkRepo.save(drink);
         return "redirect:/drink";
     }
